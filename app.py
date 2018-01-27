05bde179c4804f6a964b56f690fdc7b5e07d3aff
jaimedom <jaimedominguezdp@gmail.com>
2018-01-27 22:16:16 +0000

    Class root

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from app.Transformers import ColumnSelectTransformer, DayProcessor, MonthProcessor, CountyDicGenerator, ThresholdEstimator
+from capstone.Transformers import ColumnSelectTransformer, DayProcessor, MonthProcessor, CountyDicGenerator, ThresholdEstimator
 import pandas as pd
 from bokeh.plotting import figure
 from bokeh.embed import components
