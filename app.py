157133d9e9bb2a2497813870213212edc7b5245a
jaimedom <jaimedominguezdp@gmail.com>
2018-02-02 16:42:44 +0000

    Adding historic functionality

--- app.py
+++ app.py
@@ -153,6 +153,31 @@ model_final = Pipeline([
 model = ThresholdEstimator(model_final,0.1)
 model.fit(df2,label2)
 
+# Define terms dictionary
+    
+term ={
+        'Cost':'cost',
+        'Acres':'burnt_acres',
+        'Injuries':'injuries',
+        'Destroyed structures':'destroyedStructures',
+        'Thretened structures':'threatenedStructures',
+        'Airtankers':'airtankers',
+        'Bulldozers':'dozers',
+        'Fire engines':'fireEngines',
+        'Fire personnel':'firePersonnel',
+        'Firemen':'fireCrews',
+        'Helicopters':'helicopters',
+        'Water tenders':'waterTenders',
+        'Maximum temperature':'maxTemp',
+        'Mean temperature':'meanTemp',
+        'Minimum temperature':'minTemp',
+        'Dew point':'dewPoint',
+        'Maximum humidity':'maxHumidity',
+        'Mean humidity':'avgHumidity',
+        'Minimum humidity':'minHumidity',
+        'Maximum wind':'maxWind'
+}
+
 @app.route('/')
 def index():
     
@@ -170,78 +195,46 @@ def historic():
 
     df = pd.read_csv('fires_final.csv')
     
-    # Define terms dictionary
+    # Obtain input
     
-    term ={
-           'Cost':'cost',
-           'Acres':'burnt_acres',
-           'Injuries':'injuries',
-           'Destroyed structures':'destroyedStructures',
-           'Thretened structures':'threatenedStructures',
-           'Airtankers':'airtankers',
-           'Bulldozers':'dozers',
-           'Fire engines':'fireEngines',
-           'Fire personnel':'firePersonnel',
-           'Firemen':'fireCrews',
-           'Helicopters':'helicopters',
-           'Water tenders':'waterTenders',
-           'Maximum temperature':'maxTemp',
-           'Mean temperature':'meanTemp',
-           'Minimum temperature':'minTemp',
-           'Dew point':'dewPoint',
-           'Maximum humidity':'maxHumidity',
-           'Mean humidity':'avgHumidity',
-           'Minimum humidity':'minHumidity',
-           'Maximum wind':'maxWind'
-           }
+    selected_county = request.args.get('selected_county')
+    if selected_county == None:
+        selected_county = 'Alameda'
+        
+    selected_x = request.args.get('selected_x')
+    if selected_x == None:
+        selected_x = 'Mean temperature'
+        
+    selected_y = request.args.get('selected_y')
+    if selected_y == None:
+        selected_y = 'Acres'
     
     # Create the plot
     
-    selected_county = 'Alameda'
-    selected_x = 'Mean temperature'
-    selected_y = 'Acres'
     x = list(df[df['county']==selected_county][term[selected_x]]) 
     y = list(df[df['county']==selected_county][term[selected_y]])
     
     source = ColumnDataSource(data=dict(x=x, y=y))
     
-    plot = figure(plot_height=400, plot_width=400, title="Historic fire-related data",
+    plot = figure(plot_height=400, plot_width=400, 
+                  title=selected_x+' vs '+selected_y+' in '+selected_county+' County',
                   tools="pan,reset,save,wheel_zoom")
     
     plot.circle('x', 'y', source=source)
     
-    # Create the widgets
+    # Create the widgets    
     
-    menu1 = sorted(df['county'].unique())
-    menu2 = sorted(term.keys())
-    
-    select1 = Select(title="County:", value="Alameda", options=menu1)
-    select2 = Select(title="Variable X:", value="Mean temperature", options=menu2)
-    select3 = Select(title="Variable Y:", value="Acres", options=menu2)
-    
-    # Create callback
-    
-    def update_data(attrname, old, new):
-        
-        selected_county = select1.value
-        selected_x = select2.value
-        selected_y = select3.value
-        x = list(df[df['county']==selected_county][term[selected_x]]) 
-        y = list(df[df['county']==selected_county][term[selected_y]])
-    
-        source.data = dict(x=x, y=y)
-        
-    for w in [select1, select2, select3]:
-        w.on_change('value', update_data)
-    
-    # Create layout
-        
-    inputs = widgetbox(select1,select2,select3)
-    layout = column(inputs, plot)
-    
-    script, div = components(layout)
+    script, div = components(plot)
    
-    return render_template("historic.html", script=script, div=div)
+    return render_template("historic.html", 
+                           script=script, 
+                           div=div,
+                           selected_county=selected_county,
+                           selected_x=selected_x,
+                           selected_y=selected_y,
+                           county_names=sorted(list(set(df['county']))),
+                           axis_options = sorted(list(term.keys())))
+
 
 @app.route('/map')
 def map():
